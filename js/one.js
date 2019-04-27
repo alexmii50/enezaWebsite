3b4f2f7b27a99fa0acc4841511ae8693eaf286ab
alexmii50 <alexmurii50@gmail.com>
2019-04-27 11:43:08 +0300

    your message goes here

--- js/one.js
+++ js/one.js
@@ -57,17 +57,31 @@ $("#nav a").on('click', function(event) {
     });
 
     //smooth scrolling
-    $(function(){
-      $('a.smooth-scroll').click(function(event){
+    // $(function(){
+    //   $('a.smooth-scroll').click(function(event){
+    //     event.preventDefault();
+    //     var section_id = $(this).attr("href");
+
+    //     $("html, body").animate({
+    //       scrollTop: $(section_id).offset().top -64
+    //     }, 1250, "easeInBack");
+
+    //   });
+    // } );
+
+    $("a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
         event.preventDefault();
-        var section_id = $(this).attr("href");
-
-        $("html, body").animate({
-          scrollTop: $(section_id).offset().top -64
-        }, 1250, "easeInBack");
-
-      });
-    } );
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } // End if
+    });
 
     $(window).on('load', function(){
       $('#arrow-down i').addClass("animated fadeInDown infinite");
